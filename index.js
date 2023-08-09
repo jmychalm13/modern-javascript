76c854645f610c338d0e2e5c5f627ccc734ed73a
“Jessica <jmychalm13@gmail.com>
2023-08-08 19:51:42 -0500

    added webpack and .gitignore for node modules

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var moment = require("moment");
+
 console.log("Hello from Javscript!");
-var datetime = moment().startOf("day").fromNow();
+var datetime = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(datetime);
